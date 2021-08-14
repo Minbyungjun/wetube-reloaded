c02363173ec091de9e727ca6f56adfe596a7609e
Minbyungjun <ladyfirst65@gmai.com>
2021-08-14 22:51:53 +0900

    wetube clone(8/14)_2

---
+++ README.md
@@ -0,0 +1,20 @@
+# Wetube Reloaded
+
+=== 글로벌 라우터 ===
+/ -> Home
+/join -> Join
+/login -> Login
+/search -> Search
+
+=== user 관리 부분 ===
+
+/users/edit -> Edit user
+/users/delete -> Delete user
+
+=== 영상 관리부분 ===
+
+/videos/watch -> Watch Video
+/videos/edit -> Edit Video
+/videos/dlete -> Delete Video
+/videos/comments -> Comment on a video
+/videos/comments/delete -> Delete A Comment of a Video
--- src/server.js
+++ src/server.js
@@ -1,24 +1,37 @@
+//express 및 morgan middleWare 부르는곳
 import express from "express";
 import morgan from "morgan";
 
-
+//port 설정
 const PORT = 4000;
+
 const app = express();
+//만들어진 middleWare morgan
 const logger = morgan("dev");
-
-const handleHome = (req, res) => {
-    console.log("I will respond.")
-    return res.send("hello!!");
-};
-
-const loginlogger = (req,res) => {
-    return res.send("login");
-}
-
 app.use(logger); 
-app.get("/", handleHome);
-app.get("/log",loginlogger);
 
+// 라우터 만드는 곳
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req,res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
