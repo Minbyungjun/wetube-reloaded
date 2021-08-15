4ffd0a9e8781ba5f3c20e390bdb9a55d97c81cff
Minbyungjun <ladyfirst65@gmai.com>
2021-08-15 22:28:45 +0900

    router&controller(8/15)

---
+++ src/controllers/videoController.js
@@ -0,0 +1,4 @@
+export const trending = (req, res) => res.send("Home page videos");
+export const watch = (req, res) => res.send("Watch Video");
+export const edit = (req, res) => res.send("Edit Video");
+
--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 //express 및 morgan middleWare 부르는곳
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+
 
 //port 설정
 const PORT = 4000;
@@ -9,25 +13,6 @@ const app = express();
 //만들어진 middleWare morgan
 const logger = morgan("dev");
 app.use(logger); 
-
-// 라우터 만드는 곳
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req,res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/users", userRouter);
